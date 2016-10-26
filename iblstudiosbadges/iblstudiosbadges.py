b3d59d96663bf54b175d8ce5bd279b0f655d6d51
sils <sils@zeus.midworld-networks.com>
2016-10-26 13:47:52 +0200

    format support for new courses (course-v1)

--- iblstudiosbadges/iblstudiosbadges.py
+++ iblstudiosbadges/iblstudiosbadges.py
@@ -9,7 +9,7 @@ from xblock.fields import Scope, Integer, String
 from xblock.fragment import Fragment
 from xmodule.fields import RelativeTime
 
-import edxappCourseData
+import edxappBadges
 import badges_client
 
 class IBLstudiosbadges(XBlock):
@@ -20,41 +20,45 @@ class IBLstudiosbadges(XBlock):
     # xblock module name is the name of the field set in mongodb
     xblock_name_field = 'iblstudiosbadges'
     # mongodb data
-    xblock_mongodb_xmoduledb    = 'edxapp'
-    xblock_mongodb_modulestore  = 'modulestore'
+    xblock_mongodb_xmoduledb = 'edxapp'
+    xblock_mongodb_modulestore = 'modulestore'
+    xblock_mongodb_modulestore_structures = 'modulestore.structures'
+    xblock_mongodb_modulestore_activevers = 'modulestore.active_versions'
+    xblock_mongodb_modulestore_definitions = 'modulestore.definitions'
+    
     # mysql data
-    mysql_database  = 'edxapp'
-    mysql_user      = 'root'
-    mysql_pwd       = ''
+    mysql_database = 'edxapp'
+    mysql_user = 'root'
+    mysql_pwd = ''
     # general data
-    display_name    = String(display_name="Display Name", default="IBL Studios Badges", scope=Scope.settings, help="Name of the component in the edxplatform")
-    form_text       = String(display_name="text_desc", default=" ", scope=Scope.content, help="Badge text description")
+    display_name = String(display_name="Display Name", default="IBL Studios Badges", scope=Scope.settings, help="Name of the component in the edxplatform")
+    form_text = String(display_name="text_desc", default=" ", scope=Scope.content, help="Badge text description")
     congratulations_text = String(display_name="congratulations_desc", default=" ", scope=Scope.content, help="Congratulations text description")
-    enough_text     = String(display_name="enough_desc", default=" ", scope=Scope.content, help="Not-enough-score text description")
-    bg_id           = String(display_name="id", default="1", scope=Scope.content, help="Id of the Badge")
-    n_course_id     = String(display_name="CourseId", default="0", scope=Scope.user_state, help="Id of teh current course")
-    n_user_id       = String(display_name="UserId", default="0", scope=Scope.user_state, help="Id of the current user")
-    user_score      = String(display_name="UserScore", default="0", scope=Scope.user_state, help="Current section user score")
-    required_score  = String(display_name="RequiredScore", default="50", scope=Scope.content, help="Requireds core")
-    debug_mode      = String(display_name="debug", default="0", scope=Scope.content, help="Enable debug mode")
+    enough_text = String(display_name="enough_desc", default=" ", scope=Scope.content, help="Not-enough-score text description")
+    bg_id = String(display_name="id", default="1", scope=Scope.content, help="Id of the Badge")
+    n_course_id = String(display_name="CourseId", default="0", scope=Scope.user_state, help="Id of teh current course")
+    n_user_id = String(display_name="UserId", default="0", scope=Scope.user_state, help="Id of the current user")
+    user_score = String(display_name="UserScore", default="0", scope=Scope.user_state, help="Current section user score")
+    required_score = String(display_name="RequiredScore", default="50", scope=Scope.content, help="Requireds core")
+    debug_mode = String(display_name="debug", default="0", scope=Scope.content, help="Enable debug mode")
     # badges provider data
-    claim_prov_usr          = String(display_name="ProviderUSER", default="provider_user", scope=Scope.content, help="Badge provider user")
-    claim_prov_pwd          = String(display_name="ProviderPass", default="provider_key_secret", scope=Scope.content, help="Badge provider pass")
+    claim_prov_usr = String(display_name="ProviderUSER", default="provider_user", scope=Scope.content, help="Badge provider user")
+    claim_prov_pwd = String(display_name="ProviderPass", default="provider_key_secret", scope=Scope.content, help="Badge provider pass")
     # badges xblock data
     claim_name = String(display_name="ClaimUserName", default="Jhon", scope=Scope.user_state, help="")
     claim_mail = String(display_name="ClaimUserMail", default="users@email.local", scope=Scope.user_state, help="")
-    claim_db_user_data   = 'None'
-    claim_db_user_id     = 'None'
+    claim_db_user_data = 'None'
+    claim_db_user_id = 'None'
     claim_db_user_course = 'None'
-    claim_db_user_name   = 'None'
-    claim_db_user_email  = 'None'
-    claim_db_user_score  = '0'
+    claim_db_user_name = 'None'
+    claim_db_user_email = 'None'
+    claim_db_user_score = '0'
     # control errors
-    claim_badge_errors   = ''
+    claim_badge_errors = ''
     iblclient = None
     iblsettings = {
         'module':'iblstudiosbadges.BadgeOne_client.BadgeOneClient',
-        'base_url': "http://vm6-cloud.iblstudios.com:5000",
+        'base_url': "https://badgeone.com",
     }
 
     def __init__(self, *args, **kwargs):
@@ -73,9 +77,9 @@ class IBLstudiosbadges(XBlock):
         """
 
         # Setup data to claim a badge
-        self.n_user_id          = self.get_student_id()
+        self.n_user_id = self.get_student_id()
         self.claim_db_user_data = self.DB_get_user_data()
-        self.claim_db_user_id   = self.claim_db_user_data[0]
+        self.claim_db_user_id = self.claim_db_user_data[0]
         self.claim_db_user_course = self.claim_db_user_data[1]
         self.claim_db_user_name = self.claim_db_user_data[2]
         self.claim_db_user_email = self.claim_db_user_data[3]
@@ -89,25 +93,25 @@ class IBLstudiosbadges(XBlock):
         claim_name = self.claim_db_user_name
         claim_mail = self.claim_db_user_email
         self.claim_badge_errors = self.claim_badge_errors
-        self.claim_badge_form   = ''
-        self.check_earned  = ''
+        self.claim_badge_form = ''
+        self.check_earned = ''
         self.preview_badge = ''
         # Check out provider badge data
         if self.claim_badge_errors == "":
             self.claim_prov_access_token = self.iblclient.get_auth_token(self.claim_prov_usr, self.claim_prov_pwd)
-            if self.claim_prov_access_token !="":
-                badge_info = self.iblclient.get_badge_data(self.claim_prov_access_token,self.bg_id,'info')
-                badge_params = self.iblclient.get_badge_data(self.claim_prov_access_token,self.bg_id,'params')
-                obj_badge = self.iblclient.create_obj_badge(badge_info,badge_params)
-                self.check_earned = self.iblclient.check_earn_badge(self.claim_prov_access_token,self.claim_db_user_email,self.bg_id)
+            if self.claim_prov_access_token != "":
+                badge_info = self.iblclient.get_badge_data(self.claim_prov_access_token, self.bg_id, 'info')
+                badge_params = self.iblclient.get_badge_data(self.claim_prov_access_token, self.bg_id, 'params')
+                obj_badge = self.iblclient.create_obj_badge(badge_info, badge_params)
+                self.check_earned = self.iblclient.check_earn_badge(self.claim_prov_access_token, self.claim_db_user_email, self.bg_id)
                 self.preview_badge = self.iblclient.build_badge_preview(obj_badge)
                 if obj_badge :
                     # check if user has been awarded yet
-                    if self.check_earned!='':
-                        self.award_earn_prov = self.iblclient.get_award_result ( self.check_earned )
-                        self.award_earned = self.iblclient.get_award_result_formatted(self.award_earn_prov,self.congratulations_text)
+                    if self.check_earned != '':
+                        self.award_earn_prov = self.iblclient.get_award_result (self.check_earned)
+                        self.award_earned = self.iblclient.get_award_result_formatted(self.award_earn_prov, self.congratulations_text)
                     else:
-                        self.claim_badge_form = self.iblclient.build_badge_form(self.claim_db_user_name,self.claim_db_user_email,self.form_text,obj_badge)
+                        self.claim_badge_form = self.iblclient.build_badge_form(self.claim_db_user_name, self.claim_db_user_email, self.form_text, obj_badge)
                 else:
                     self.claim_badge_errors = 'Could not retrieve the information associated with the Badge ID selected. Please, verify your data.'
             else:
@@ -121,13 +125,13 @@ class IBLstudiosbadges(XBlock):
                 if int(self.user_score) < int(self.required_score):
                     html = self.resource_string("static/html/student_view_noscore.html")
                 else:
-                    if self.check_earned!='':
+                    if self.check_earned != '':
                         html = self.resource_string("static/html/student_view_earn_badge.html")
                     else:
                         html = self.resource_string("static/html/student_view_claim_badge.html")
             frag = Fragment(html.format(self=self))
             frag.add_css(self.resource_string("static/css/style.css"))
-            if self.check_earned =='':
+            if self.check_earned == '':
                 frag.add_javascript(self.resource_string("static/js/src/student_view_scripts.js"))
             frag.initialize_js('StudentViewBadge')
         else:
@@ -156,72 +160,7 @@ class IBLstudiosbadges(XBlock):
         """
         Get student data from the DB
         """
-
-        # the appmysqldb module will only be used here
-        import appmysqldb
-        # init default data
-        user_id    = "None"
-        course_id  = "None"
-        user_name  = "None"
-        user_email = "None"
-        user_score = "0"
-        # user and course
-        db = appmysqldb.mysql('localhost', 3306, self.mysql_database, self.mysql_user, '')
-        q = "SELECT id, user_id, course_id FROM student_anonymoususerid WHERE anonymous_user_id='" + self.n_user_id + "'"
-        db.query(q)
-        res = db.fetchall()
-        for row in res:
-            user_id   = row[1]
-            course_id = row[2]
-        # username
-        q = "SELECT name FROM auth_userprofile WHERE user_id='%s' " % (user_id)
-        db.query(q)
-        res = db.fetchall()
-        for row in res:
-            user_name   = row[0]
-        # email
-        q = "SELECT email FROM auth_user WHERE id='%s' " % (user_id)
-        db.query(q)
-        res = db.fetchall()
-        for row in res:
-            user_email   = row[0]
-        # course data from mongodb
-        from pymongo import Connection
-        xmoduledb   = self.xblock_mongodb_xmoduledb
-        connection  = Connection()
-        db_mongo    = connection[xmoduledb]
-        mongo_modulestore   = db_mongo[self.xblock_mongodb_modulestore]
-        badge_list_problems = edxappCourseData.getListProblemsFromBadgeId(mongo_modulestore,self.bg_id,course_id, self.xblock_name_field)
-        badge_problems_score= edxappCourseData.getScoreFromBadgeId(mongo_modulestore,self.bg_id,course_id,  self.xblock_name_field)
-        # calculate badge_score
-        user_score = 0
-        partial_user_score = []
-        badge_partial_user_score = 0
-        badge_percent_user_score = 0
-        # calculate user partials
-        if badge_problems_score>0:
-            if len(badge_list_problems)>0:
-                for problem in badge_list_problems:
-                    if 'problem_score' in problem:
-                        problem_score     = problem['problem_score']
-                        problem_id     = problem['problem_id']
-                        # partial values
-                        if int(problem_score)>0:
-                            q = "SELECT ((%s/max_grade)*grade) FROM courseware_studentmodule WHERE course_id='%s' AND student_id='%s' AND module_id='%s'" % (problem_score,course_id,user_id,problem_id)
-                            db.query(q)
-                            res = db.fetchall()
-                            for row in res:
-                                if row[0]>0:
-                                    partial_user_score.append( float(row[0]) )
-                        badge_partial_user_score = sum(partial_user_score)
-        # calculate total percent
-        if round(badge_partial_user_score,2)>0 and int(badge_problems_score)>0:
-            badge_percent_user_score = ( badge_partial_user_score * 100.0 ) / badge_problems_score
-            badge_percent_user_score = round(badge_percent_user_score,2)
-        if int(badge_percent_user_score)>0:
-            user_score = badge_percent_user_score
-        # show results
-        results = [user_id,course_id,user_name,user_email,user_score,badge_list_problems,badge_problems_score,badge_partial_user_score,badge_percent_user_score,badge_problems_score]
+        results = edxappBadges.resultBadgesScores(self)
         return results
 
     def studio_view(self, context=None):
@@ -237,7 +176,7 @@ class IBLstudiosbadges(XBlock):
         return frag
 
     @XBlock.json_handler
-    def student_claim_save(self,claimdata,suffix=''):
+    def student_claim_save(self, claimdata, suffix=''):
         """
         From student view claim and save a Badge
         """
@@ -248,16 +187,16 @@ class IBLstudiosbadges(XBlock):
             # connect to server and get a new token
             self.claim_prov_access_token = self.iblclient.get_auth_token(self.claim_prov_usr, self.claim_prov_pwd)
             # parse data to claim a badge
-            claimdata_dict= dict( entry.split('=') for entry in claimdata.split('&') )
+            claimdata_dict = dict(entry.split('=') for entry in claimdata.split('&'))
             award_data = self.iblclient.set_form_data_to_award(claimdata_dict)
-            set_award_single = self.iblclient.claim_and_award_single_badge(self.claim_prov_access_token,award_data)
+            set_award_single = self.iblclient.claim_and_award_single_badge(self.claim_prov_access_token, award_data)
             # mode debug to get provider response
             if self.debug_mode == "1":
                 debug_result = set_award_single
                 return { 'result' : debug_result }
             # result from earn request
-            award_result_prov = self.iblclient.get_award_result ( ast.literal_eval(set_award_single) )
-            award_result = self.iblclient.get_award_result_formatted(award_result_prov,self.congratulations_text)
+            award_result_prov = self.iblclient.get_award_result (ast.literal_eval(set_award_single))
+            award_result = self.iblclient.get_award_result_formatted(award_result_prov, self.congratulations_text)
         return { 'result' :  award_result }
 
     @XBlock.json_handler
